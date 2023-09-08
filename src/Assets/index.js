2c92a9afaf78c87b09b2ca619a5f46949fd929f4
karen <karen.bussines@hotmail.com>
2023-09-07 18:43:52 -0600

    Feat: create Home page

---
+++ src/Assets/index.js
@@ -0,0 +1,3 @@
+export { default as Background } from "./Background.jpeg";
+export { default as CoffeGrains } from "./coffe-grains.png";
+export { default as Logo } from "./Logo.png";
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import NavBar from "./Components";
+import { Logo } from "../../Assets";
+import "./styles.scss";
 
 function Home() {
   return (
     <div className="home">
-      <h1>Home</h1>
+      <div className="overlay"></div>
+      <div className="content">
+        <NavBar />
+        <div className="logo-image-container">
+          <img className="logo-image" src={Logo} alt="logo" />
+        </div>
+        <div className="description-container">
+          <h4>Welcome to Expresso Coffee!</h4>
+          <p>
+            Explore our menu, place orders, and manage your purchases. Discover
+            the perfect coffee experience with us.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
