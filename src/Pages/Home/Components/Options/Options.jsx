3230675087567634d37490481fe086f56f5f7ff7
karen <karen.bussines@hotmail.com>
2023-09-09 14:05:09 -0600

    Fix: bug at navbar router

--- src/Pages/Home/Components/Options/Options.jsx
+++ src/Pages/Home/Components/Options/Options.jsx
@@ -5,7 +5,7 @@ import "./styles.scss";
 function Options() {
   return (
     <div className="Options">
-      <Link to="/orders" className="Options-option">
+      <Link to="/menu" className="Options-option">
         MENU
       </Link>
       <Link to="/orders" className="Options-option">
