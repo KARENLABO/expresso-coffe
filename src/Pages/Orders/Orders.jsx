ed9877af0bbbd1f748fe17d9910871b2a7dbc9b3
karen <karen.bussines@hotmail.com>
2023-09-09 22:13:24 -0600

    Refactor: Orders page

--- src/Pages/Orders/Orders.jsx
+++ src/Pages/Orders/Orders.jsx
@@ -1,10 +1,28 @@
-import { Content } from "../../Components";
+import { Content, Table } from "../../Components";
+import orders from "../../data/orders.json";
 import "./styles.scss";
+import { transformData } from "../../Helpers";
+
+const columns = [
+  { field: "user", title: "CUSTOMER" },
+  { field: "drink", title: "DRINK" },
+  { field: "size", title: "SIZE" },
+];
 
 function Orders() {
+  // Transform the data before passing it to the table
+  const transformedOrders = transformData(
+    orders,
+    "drink", // The field to verify
+    "supermochacrapucaramelcream", // The value to transform
+    "Mocha caramel cream" // The expected value
+  );
+
   return (
     <div className="orders">
-      <Content title="COFEE ORDERS"></Content>
+      <Content title="COFFEE ORDERS">
+        <Table data={transformedOrders} columns={columns} />
+      </Content>
     </div>
   );
 }
