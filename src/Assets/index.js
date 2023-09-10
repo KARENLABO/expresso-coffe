89f64caeae575a382dbb154c534b7c4c968d785b
karen <karen.bussines@hotmail.com>
2023-09-09 22:05:52 -0600

    Feat: create new svg's to use in tables

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -1,3 +1,7 @@
 export { default as Background } from "./Background.jpeg";
 export { default as CoffeGrains } from "./coffe-grains.png";
 export { default as Logo } from "./Logo.png";
+export { default as ChevronLeft } from "./chevronLeft.svg";
+export { default as ChevronRight } from "./chevronRight.svg";
+export { default as ChevronUp } from "./chevronUp.svg";
+export { default as ChevronDown } from "./chevronDown.svg";
