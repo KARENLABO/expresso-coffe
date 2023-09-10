911a806801fa761a0af68a97e2289d06370de8b7
karen <karen.bussines@hotmail.com>
2023-09-09 22:10:02 -0600

    Refactor: Update menu table

--- src/Pages/Menu/Components/Table/Table.jsx
+++ src/Pages/Menu/Components/Table/Table.jsx
@@ -1,26 +1,32 @@
-import { capitalizeFirstLetter } from "../../../../Helpers";
+import { capitalizeFirstLetter, transformData } from "../../../../Helpers";
 import ProductSize from "../ProductSize";
 import "./styles.scss";
 
 function Table({ products }) {
+  // Transform drink names before displaying them
+  const transformedProducts = transformData(
+    products,
+    "drink_name", // The field to check
+    "supermochacrapucaramelcream", // The value to transform
+    "Mocha caramel cream" // The expected value
+  );
+
   return (
     <div className="menu-table">
-      {products ? ( // Conditionally render only when 'loading' is false and 'products' is not null or undefined.
+      {transformedProducts ? ( // Conditionally render only when 'loading' is false and 'products' is not null or undefined.
         <div className="menu-container-products">
-          {/* Map over the 'products' array.*/}
-          {products.map((product) => {
+          {/* Map over the 'transformedProducts' array. */}
+          {transformedProducts.map((product) => {
             const { drink_name, prices } = product;
             const pricesArray = Object.entries(prices); // Convert 'prices' object into an array of key-value pairs to be able to iterate after.
 
             return (
               <div className="product-container" key={drink_name}>
                 <h3 className="menu-product-title">
-                  {drink_name === "supermochacrapucaramelcream"
-                    ? " Mocha caramel cream"
-                    : capitalizeFirstLetter(drink_name)}
+                  {capitalizeFirstLetter(drink_name)}
                 </h3>
 
-                {/* Map over the 'prices' array.*/}
+                {/* Map over the 'prices' array. */}
                 {pricesArray.map(([sizeName, price]) => (
                   <ProductSize
                     key={sizeName}
