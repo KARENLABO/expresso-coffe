717159db8a88f2195896490348d2a82e4c1cb2c9
karen <karen.bussines@hotmail.com>
2023-09-09 22:13:05 -0600

    Feat: export new helper functions

--- src/Helpers/index.js
+++ src/Helpers/index.js
@@ -1,1 +1,4 @@
 export { default as capitalizeFirstLetter } from "./capitalizeFirstLetter.js";
+export { default as paginateArray } from "./paginateArray.js";
+export { default as calculatePages } from "./calculatePages.js";
+export { default as transformData } from "./transformData.js";
