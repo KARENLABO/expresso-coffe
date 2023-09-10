b9040cd0e9619a1cd8aa16988e4dde802efbdbda
karen <karen.bussines@hotmail.com>
2023-09-10 14:49:22 -0600

    Refactor: remove table from general componets, because was adapted only to orders page

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,4 +1,3 @@
 export { default as NavBar } from "./NavBar";
 export { default as Content } from "./Content";
 export { default as Pagination } from "./Pagination";
-export { default as Table } from "./Table";
