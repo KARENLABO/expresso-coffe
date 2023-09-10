4e661d207564e8bdd32a64416b744e4743f105c1
karen <karen.bussines@hotmail.com>
2023-09-10 14:51:22 -0600

    Refactor: Update exports

--- src/Helpers/index.js
+++ src/Helpers/index.js
@@ -2,3 +2,5 @@ export { default as capitalizeFirstLetter } from "./capitalizeFirstLetter.js";
 export { default as paginateArray } from "./paginateArray.js";
 export { default as calculatePages } from "./calculatePages.js";
 export { default as transformData } from "./transformData.js";
+export { default as addNewPropertiesToJson } from "./addNewPropertiesToJson.js";
+export { default as groupOrdersByUser } from "./groupOrdersByUser.js";
