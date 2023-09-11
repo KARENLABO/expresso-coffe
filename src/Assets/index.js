992ec540d438e728b0b58da2730b959f6a9d1f84
karen <karen.bussines@hotmail.com>
2023-09-11 01:00:27 -0600

    Feat: create 404 page

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -5,3 +5,4 @@ export { default as ChevronLeft } from "./chevronLeft.svg";
 export { default as ChevronRight } from "./chevronRight.svg";
 export { default as ChevronUp } from "./chevronUp.svg";
 export { default as ChevronDown } from "./chevronDown.svg";
+export { default as PageNotFound } from "./PageNotFound.png";
--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { PageNotFound } from "../../Assets";
+import "./styles.scss";
 
 function Error() {
   return (
-    <div className="error">
-      <h1>UPS!</h1>
-      <h1>SOMETHING WENT WRONG!</h1>
+    <div className="error-page">
+      <img src={PageNotFound} alt="Error" />
+      <h1>404</h1>
+      <p>Oops! Something went wrong.</p>
+      <a href="/">Go to the homepage</a>
     </div>
   );
 }
